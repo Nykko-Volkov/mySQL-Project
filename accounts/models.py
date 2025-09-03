547caee2e40557fd3792e8abf141d997cb69f3f7
Nykko-Volkov <nathannunesmoreira1@gmail.com>
2025-09-03 15:27:11 -0300

    .

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser): # inherent all properties of AbstractUser to make custom user model
     class Role(models.TextChoices): # to create a role field with 4 choices
         ADMIN = 'ADMIN', 'Admin'
-        MANAGER = 'MANAGER',"Manage"
+        MANAGER = 'MANAGER',"Manager"
         CASHIER = 'CASHIER', 'Cashier'
         STAFF = 'STAFF', 'Staff'
     role = models.CharField(max_length=10, choices=Role.choices) # role field with max length 10 and choices from Role class
