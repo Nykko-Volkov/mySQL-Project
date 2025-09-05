6f3408d98aaac5795fa095f5fe9fe67f0a8e8e85
Nykko-Volkov <nathannunesmoreira1@gmail.com>
2025-09-05 15:22:41 -0300

    updated

---
+++ orders/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Order, OrderItem
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['customer', 'status', 'order_type', 'placed_by']
+
+class OrderItemForm(forms.ModelForm):
+    class Meta:
+        model = OrderItem
+        fields = ['order', 'product', 'quantity']
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import OrderForm, OrderItemForm
 # Create your views here.
+
+
+def create_order(request):
+    if request.method == "POST":
+        order_form = OrderForm(request.POST)
+        order_item_form = OrderItemForm(request.POST)
+
+        if order_form.is_valid() and order_item_form.is_valid():
+            order = order_form.save()
+            order_item = order_item_form.save(commit=False)
+            order_item.order = order
+            # Auto-fill price & total
+            order_item.unit_price = order_item.product.price
+            order_item.line_total = order_item.quantity * order_item.unit_price
+            order_item.save()
+            return redirect("create_order")  # stay on same page
+    else:
+        order_form = OrderForm()
+        order_item_form = OrderItemForm()
+
+    return render(
+        request,
+        "orders/create_order.html",
+        {"order_form": order_form, "item_form": order_item_form},
+    )
